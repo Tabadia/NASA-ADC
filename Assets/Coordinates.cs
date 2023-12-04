7b71480477490414d91141536a4d840b9c9f8861
edwinSp1 <115381002+edwinSp1@users.noreply.github.com>
2023-12-04 14:44:34 -0800

    Update Coordinates.cs to show more data and take into account real-world situations (NOT COMPLETED))

--- Assets/Coordinates.cs
+++ Assets/Coordinates.cs
@@ -10,15 +10,45 @@ public class Coordinates : MonoBehaviour
     public GameObject p2;
     public GameObject coordinates;
 
+    public Vector3 peakNearShackletonCoords = new Vector3(0, 0, 0); //someone who knows, pls do this
 
     TextMeshProUGUI coordText;
     void Start()
     {
         coordText = coordinates.GetComponent<TextMeshProUGUI>();
     }
+    float getDistFromEarth(Vector3 pos) {
+        return Vector3.distance(p1Pos, new Vector3(361000, 0, -42100));
+    }
+    double getAngleFromEarth(float height, float distance) {
+        return Math.Atan(height / distance) * 180;
+    }
     // Update is called once per frame
     void Update()
     {
-        coordText.text = "Astronaut 1 Position:" + p1.transform.position.ToString() + '\n' + "Astronaut 2 Position:"+ p2.transform.position.ToString(); 
+        Vector3 p1Pos = new Vector3(p1.position);
+        p1Pos.y -= peakNearShackletonCoords.y;
+        Vector3 p2Pos = new Vector3(p2.position);
+        p2Pos.y -= peakNearShackletonCoords.y;
+
+        // Waiting for Alex's util file.
+        float azimuthAngle;
+
+        float p1Dist = getDistFromEarth(p1Pos);
+        
+        string astronaut1Data = "Astronaut 1 Position:" + p1Pos.ToString() + "\n"
+        + "Distance to Earth: " + p1Dist + "\n" 
+        + "Elevation angle to Earth: " + getAngleFromEarth(p1Pos.y, p1Dist) + "\n" 
+        + "Azimuth angle: " + "NOT IMPLEMENTED YET";
+
+        float p2Dist = getDistFromEarth(p2Pos);
+        
+        string astronaut2Data = "Astronaut 2 Position:" + p2Pos.ToString() + "\n"
+        + "Distance to Earth: " + p2Dist + "\n" 
+        + "Elevation angle to Earth: " + getAngleFromEarth(p2Pos.y, p2Dist) + "\n" 
+        + "Azimuth angle: " + "NOT IMPLEMENTED YET";
+        
+        coordText.text = astronaut1Data + "\n"
+        + astronaut2Data;
     }
 }
