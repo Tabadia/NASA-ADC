9df1ddc5b3fd00f175a5f6c4efee36da0bf380f0
Sidd_ <siddharth726@outlook.com>
2023-12-12 12:30:01 -0800

    pathfinding UI start

---
+++ Assets/PathfindingUI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingUI : MonoBehaviour
+{
+    public TextMeshProUGUI coordinateText; // Reference to your TextMeshPro object
+    public Transform groundPlane; // Reference to the ground plane (where the camera is looking down)
+
+    void Update()
+    {
+        UpdateCoordinatesOnMouseClick();
+        UpdateCoordinatesOnMouseMovement();
+    }
+
+    void UpdateCoordinatesOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0)) // Check for left mouse button click
+        {
+            Vector3 cartesianCoordinates = GetCartesianCoordinates();
+            Debug.Log("Cartesian Coordinates: " + cartesianCoordinates);
+
+            // Display coordinates on TextMeshPro object
+            if (coordinateText != null)
+            {
+                coordinateText.text = "Coordinates: " + cartesianCoordinates.ToString();
+            }
+        }
+    }
+
+    void UpdateCoordinatesOnMouseMovement()
+    {
+        Vector3 cartesianCoordinates = GetCartesianCoordinates();
+
+        // Display coordinates on TextMeshPro object during mouse movement
+        if (coordinateText != null)
+        {
+            coordinateText.text = "Coordinates: " + cartesianCoordinates.ToString();
+        }
+    }
+
+    Vector3 GetCartesianCoordinates()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Use the ground plane instead of a texture
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
+        {
+            return hit.point;
+        }
+
+        return Vector3.zero;
+    }
+}
